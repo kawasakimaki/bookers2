13ea2fd264de0d52fbdd0e4bf9fdc8ccd8c121ff
kawasakimaki <kchurip.08@gmail.com>
2023-08-19 18:30:44 +0000

    [Add]sign upページの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root :to => 'homes#top'
-  get 'home/about' => 'home#about'
+  get 'home/about' => 'homes#about'
 
   resources :users, only: [:edit, :index, :show, :update]
   resources :books
